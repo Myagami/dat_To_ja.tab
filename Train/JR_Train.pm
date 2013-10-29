9044e2562475fb3c36504e83906a3d07af7d7842
M.Yagami <hayate@Reinforce-Eins.(none)>
2013-10-29 17:52:08 +0900

    dc support start

--- Train/JR_Train.pm
+++ Train/JR_Train.pm
@@ -25,6 +25,8 @@ sub campany{#会社名を取得する
 }
 
 sub type{#車両の種別取得
+    #格納変数
+    my $data = null ;
     #形式
     my $types = {
 	T => "サハ",
@@ -36,32 +38,36 @@ sub type{#車両の種別取得
 
     #$self->{name} =~ /^[A-z]{1,} ([A-Za-z0-9_']{1,})(.*)/ ;
     $self->{name} =~ s/\'//g ;
-    #形式部分を取得
-    $self->{name} =~ /^[A-z]{1,} ([A-Za-z0-9_']{1,}|[A-Za-z0-9_]{1,})(.*)/ ;
 
-    #形式部分を分割
-    #list(my $type,my $num,my $serial) = split(/_/,$1) ;
-    my @type = split(/_/,$1) ;
-    my $data = null ;
-    $type[0] =~ s/[0-9]{1,}//g ;
-    
-    #print join("/",@type)."\n" ;
-    
-    #print $types->{$type[0]} ;
-    $data = $types->{$type[0]} ;
-    $data .=  $type[1] ;
-    $data .= "系" ;
-    
-    if(defined($type[2])){
-	$data .=  $type[2] ;
-	$data .= "番台" ;
-    }
-    
-    if(defined($2)){
-	$data .=  $2 ;
+    if($self->{name} =~ /Kiha/){
+
+    }else{
+	#形式部分を取得
+	$self->{name} =~ /^[A-z]{1,} ([A-Za-z0-9_']{1,}|[A-Za-z0-9_]{1,})(.*)/ ;
+
+	#形式部分を分割
+	#list(my $type,my $num,my $serial) = split(/_/,$1) ;
+	my @type = split(/_/,$1) ;
+	$type[0] =~ s/[0-9]//g ;
+	
+	#print join("/",@type)."\n" ;
+	
+	#print $types->{$type[0]} ;
+	$data = $types->{$type[0]} ;
+	$data .=  $type[1] ;
+	$data .= "系" ;
+	
+	if(defined($type[2])){
+	    $data .=  $type[2] ;
+	    $data .= "番台" ;
+	}
+	
+	if(defined($2)){
+	    $data .=  $2 ;
+	}
     }
     return $data ;
-}
 
+}
 
 1;
